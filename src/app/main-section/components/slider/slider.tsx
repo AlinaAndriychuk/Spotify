243a6bc259c10843371f53e8e2f8fba1d3170163
alina <alina@coderiver.com.ua>
2021-01-20 18:30:58 +0200

    tabs

--- src/app/main-section/components/slider/slider.tsx
+++ src/app/main-section/components/slider/slider.tsx
@@ -1,11 +1,20 @@
-import React, {useState} from 'react';
-import {Tabs} from '../tabs/tabs';
+import React, { useState } from 'react';
+import { Tabs } from '../tabs/tabs';
+import { Owerview } from '../owerview/owerview';
+import { ArtistsPage } from '../related-artists-page/related-artists-page';
 
-const options = [{ value: '1', label: 'owerview'}, { value: '2', label: 'related artists'}];
+const options = [{ value: '1', label: 'owerview' }, { value: '2', label: 'related artists' }];
+const artistsPageImages = ['https://s3-us-west-2.amazonaws.com/s.cdpn.io/7022/hoodie.jpg', 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/7022/mikestud_large.jpg', 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/7022/drake_large.jpg', 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/7022/jcole_large.jpg', 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/7022/bigSean_large.jpg', 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/7022/wiz.jpeg', 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/7022/yonas.jpg', 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/7022/childish.jpg'];
+const artistsPageNames = ['Hoodie Allen', 'Mike Stud', 'Drake', 'J. Cole', 'Big Sean', 'Wiz Khalifa', 'Yonas', 'Childish Gambino'];
 
 export interface TabOption {
   value: string;
   label: string;
+}
+
+enum Labels {
+  Owerview = 'owerview',
+  RelatedArtists = 'related artists'
 }
 
 export const Slider = () => {
@@ -14,13 +23,16 @@ export const Slider = () => {
   const handleOnChange = value => {
     setValue(value);
   }
-  
+
   const renderTab = label => {
     return label;
   };
 
   return (
-    <Tabs options={options} value={value} renderTab={renderTab} onChange={handleOnChange}>
-    </Tabs>
+    <>
+      <Tabs options={options} value={value} renderTab={renderTab} onChange={handleOnChange}>
+      </Tabs>
+      {value.label === Labels.Owerview ? (<Owerview/>) : (<ArtistsPage names={artistsPageNames} images={artistsPageImages}></ArtistsPage>)}
+    </>
   )
 };
--- src/app/main-section/components/tabs/tabs.tsx
+++ src/app/main-section/components/tabs/tabs.tsx
@@ -1,30 +1,25 @@
 import React, {useRef} from 'react';
 import classNames from 'classnames';
 import { TabOption } from '../slider/slider';
-import {Owerview} from '../owerview/owerview';
 import './tabs.sass';
-import {ArtistsPage} from '../related-artists-page/related-artists-page';
 
 interface TabItemProps {
   active: boolean;
   options: TabOption;
   renderTab: (label: string) => string;
-  switchTab: (value: TabOption) => void;
+  onChange: (value: TabOption) => void;
 };
-
-const artistsPageImages = ['https://s3-us-west-2.amazonaws.com/s.cdpn.io/7022/hoodie.jpg', 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/7022/mikestud.jpg', 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/7022/drake.jpeg', 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/7022/jcole.jpg', 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/7022/bigsean.jpg', 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/7022/wiz.jpeg', 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/7022/yonas.jpg', 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/7022/childish.jpg'];
-const artistsPageNames = ['Hoodie Allen', 'Mike Stud', 'Drake', 'J. Cole', 'Big Sean', 'Wiz Khalifa', 'Yonas', 'Childish Gambino'];
 
 const TabItem: React.FunctionComponent<TabItemProps> = ({
   active,
   options,
   renderTab,
-  switchTab,
+  onChange,
 }) => {
   const itemClass = classNames({"tabs__item": true, "tabs__item_active": active});
 
   return (
-    <li className={itemClass} onClick={ () => switchTab(options) }>
+    <li className={itemClass} onClick={ () => onChange(options) }>
       <p className="tabs__item-text">
         {renderTab(options.label)}
       </p>
@@ -45,35 +40,17 @@ export const Tabs: React.FunctionComponent<TabProps> = ({
   renderTab,
   onChange,
 }) => {
-  const slider = useRef<HTMLDivElement>(null);
-  const sliderPage = useRef<HTMLDivElement>(null);
-
-  function switchTab(value) {
-    const pageWidth = (sliderPage.current as HTMLDivElement).clientWidth; 
-    (slider.current as HTMLDivElement).style.right = pageWidth * (value.value - 1) + "px";
-  
-    onChange(value);
-  }
-  
   return (
     <div className="tabs">
       <ul className="tabs__list">
         {
           options.map( item => {
             return (
-              <TabItem options={item} switchTab={switchTab} active={value.value === item.value} renderTab={renderTab} key={item.value}></TabItem>  
+              <TabItem options={item} onChange={onChange} active={value.value === item.value} renderTab={renderTab} key={item.value}></TabItem>  
             )
           })
         }
       </ul>
-      <div className="slider" ref={slider}>
-        <div className="slider__page" ref={sliderPage}>
-          <Owerview></Owerview>
-        </div>
-        <div className="slider__page">
-          <ArtistsPage names={artistsPageNames} images={artistsPageImages}></ArtistsPage>
-        </div>
-      </div>
     </div>
   )
 };

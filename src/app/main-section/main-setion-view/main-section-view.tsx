b04db6bc13a0bf02de82f3418b5a9ff084f245b4
alina <alina@coderiver.com.ua>
2021-01-22 18:13:28 +0200

    overflow sections

--- src/app/main-section/main-setion-view/main-section-view.tsx
+++ src/app/main-section/main-setion-view/main-section-view.tsx
@@ -6,8 +6,10 @@ import {TabsView} from '../components/tabs-view/tabs-view'
 export const MainSection = () => {
    return (
     <div className="main">
-      <ArtistBar artistImage="https://s3-us-west-2.amazonaws.com/s.cdpn.io/7022/g_eazy_propic.jpg" artistBackground="https://s3-us-west-2.amazonaws.com/s.cdpn.io/7022/g-eazy.png" name="G-Easy" listeners="15,662,810"/>
-      <TabsView/>
+      <div className="main__content">
+        <ArtistBar artistImage="https://s3-us-west-2.amazonaws.com/s.cdpn.io/7022/g_eazy_propic.jpg" artistBackground="https://s3-us-west-2.amazonaws.com/s.cdpn.io/7022/g-eazy.png" name="G-Easy" listeners="15,662,810"/>
+        <TabsView/>
+      </div>
     </div>
   )
 };
